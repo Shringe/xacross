b0974d302b2bf752e42a3343bb60d8caffc8541b
Shringe <dashingkoso@gmail.com>
2025-02-10 20:45:25 -0600

    Flattened grid to 2d for performance and simplicity of coordinates. Implemented move generation.

--- src/wordsearch.rs
+++ src/wordsearch.rs
@@ -1,17 +1,14 @@
 use std::fs;
 use std::path::PathBuf;
-use std::io::{BufRead, BufReader, Result};
+use std::io::Result;
 
 
 #[derive(Debug)]
 pub struct WordSearch { 
-    grid: Vec<Vec<char>>,
-    bank: Vec<Vec<char>>,
-}
-
-struct Coordinate {
-    x: u16,
-    y: u16,
+    grid: Vec<char>,
+    bank: Vec<String>,
+    rows: usize,
+    cols: usize,
 }
 
 pub fn read_wordsearch(file: &PathBuf) -> Result<String> {
@@ -19,8 +16,10 @@ pub fn read_wordsearch(file: &PathBuf) -> Result<String> {
 }
 
 pub fn parse_wordsearch(raw_wordsearch: &String) -> WordSearch {
-    let mut grid = Vec::<Vec<char>>::new();
-    let mut bank = Vec::<Vec<char>>::new();
+    let mut grid = Vec::<char>::new();
+    let mut bank = Vec::<String>::new();
+    let mut rows = 0;
+    let mut cols = 0;
 
     let mut is_bank = false;
     for line in raw_wordsearch.lines() {
@@ -30,33 +29,64 @@ pub fn parse_wordsearch(raw_wordsearch: &String) -> WordSearch {
         }
 
         if is_bank {
-            bank.push(line.chars().collect());
+            bank.push(line.to_string());
         } else {
-            grid.push(line.chars().collect());
+            grid.extend(line.chars().collect::<Vec<char>>());
+
+            rows += 1;
+            if cols == 0 {
+                cols = line.len()
+            }
         }
     }
 
     WordSearch {
         grid,
         bank,
+        rows,
+        cols,
     }
 }
 
-fn generate_directions(wordsearch: &WordSearch) -> Vec<Coordinate>{
-    vec![
-        Coordinate {
-            x: 15,
-            y: 20,
-        }
-    ]
+pub fn generate_directions(wordsearch: &WordSearch, index: usize, row: usize, col: usize) -> Vec<usize> {
+    let width = wordsearch.cols;
+    let height = wordsearch.rows;
+
+    let is_first_row = row == 0;
+    let is_last_row = row == height - 1;
+    let is_first_col = col == 0;
+    let is_last_col = col == width - 1;
+
+    let mut points = Vec::<usize>::new();
+    if !is_first_row && !is_first_col { // NW
+        points.push(index - width - 1);
+    } if !is_first_row { // N
+        points.push(index - width);
+    } if !is_first_row && !is_last_col { // NE
+        points.push(index - width + 1);
+    } if !is_first_col { // W
+        points.push(index - 1);
+    } if !is_last_col { // E
+        points.push(index + 1)
+    } if !is_last_row && !is_first_col { // SW
+        points.push(index + width - 1)
+    } if !is_last_row { // S
+        points.push(index + width)
+    } if !is_last_row && !is_last_col { // SE
+        points.push(index + width + 1)
+    }
+
+    points
 }
 
 pub fn search(wordsearch: &WordSearch) {
-    for x in &wordsearch.grid {
-        for y in &wordsearch.grid {
-            for direction in generate_directions(&wordsearch) {
+    for (i, letter) in wordsearch.grid.iter().enumerate() {
+        let row = i / wordsearch.cols;
+        let col = i % wordsearch.cols;
 
-            }
+        let directions = generate_directions(wordsearch,i, row, col);
+        println!("{:?}", directions);
+        for direction in directions {
         }
     }
 }
