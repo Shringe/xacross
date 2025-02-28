bc3ea0188bdccd05a37917185b9ab4c7341f82c7
Logen Deamicis <logen.deamicis@valleyviewisd.net>
2025-02-28 13:59:09 -0600

    implementing new cli

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,28 @@ use std::fs::read_to_string;
 
 
 fn main() {
-    let args = cli::Args::parse();
-
-    let raw_wordsearch = read_to_string(&args.file).expect("Could not read file");
-    let wordsearch = WordSearch::parse(&raw_wordsearch);
-    
+    let args = cli::Cli::parse();
 
     if args.debug {
         println!("{:#?}", args);
-        println!("{:?}", wordsearch);
     }
 
-    println!("{}", wordsearch.render());
-    println!("Solving wordsearch...");
+    let raw_wordsearch = read_to_string(&args.file).expect("Could not read file");
+    let wordsearch = WordSearch::parse(&raw_wordsearch);
+
+    if args.debug {
+        //println!("{:?}", wordsearch);
+        println!("{:?}", args.commands);
+    }
+
+    match &args.commands {
+        cli::Commands::RenderOptions(options) => {
+            println!("'myapp add' was used, name is: {:?}", options.raw);
+        }
+    }
+
+    //println!("{}", wordsearch.render());
+    //println!("Solving wordsearch...");
     let solved = wordsearch.search();
-    println!("{}", solved.render(true));
+    //println!("{}", solved.render());
 }
