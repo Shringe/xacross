b37c75001957a0c4331fc00316779a67fa4a1dc5
Shringe <git@nsaria.com>
2025-12-16 09:57:47 -0600

    update flake, dependencies, version

--- flake.nix
+++ flake.nix
@@ -1,30 +1,42 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-        
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = with pkgs; [
-            rustc
-            cargo
-            git
-            fish
-          ];
 
-          shellHook = ''
-            exec fish
-          '';
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        name = cargoToml.package.name;
+        version = cargoToml.package.version;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          inherit name version;
+
+          cargoLock.lockFile = ./Cargo.lock;
+          src = self;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            rustc
+            rustfmt
+            cargo
+            rust-analyzer
+          ];
         };
       }
     );
