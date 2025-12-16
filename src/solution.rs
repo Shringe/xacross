ae3c19a7803427f33acc9d2f4afb43693ccdf203
Shringe <git@nsaria.com>
2025-12-16 08:57:04 -0600

    refactor guide rendering

--- src/solution.rs
+++ src/solution.rs
@@ -44,20 +44,8 @@ impl Solution {
     fn render_grid(&self) -> String {
         let mut grid_render: Vec<Vec<String>> = Vec::new();
 
-        let horizontal_gap = self.wordsearch.width.to_string().len();
-        //grid_render.push(vec!["_ "..self.wordsearch.width)]);
-        //
-        //grid_render.push(self.wordsearch.grid.map());
-        let mut top_bar = Vec::new(); 
-        let mut top_spacer = Vec::new();
-        for x in 0..self.wordsearch.width {
-            top_bar.push(x.to_string());
-            top_spacer.push("_".to_string());
-        }
-        //grid_render.push(top_bar);
-        //grid_render.push(top_spacer);
-
-        for (_index, row) in self.wordsearch.grid.iter().enumerate() {
+        // Converting the chars to strings for coloring
+        for row in self.wordsearch.grid.iter() {
             let mut new_row: Vec<String> = Vec::new();
             
             for &c in row {
@@ -67,7 +55,7 @@ impl Solution {
             grid_render.push(new_row);
         }
 
-        // Coloring
+        // Highlighting
         for word in &self.found {
             for point in &word.points {
                 let letter = grid_render[point.x][point.y].bright_blue().to_string().to_lowercase();
@@ -75,15 +63,30 @@ impl Solution {
             }
         }
 
+        // Adding the gaps and final formatting
         let mut rendered_grid = Vec::new();
-        let vertical_seperator = " ".repeat(horizontal_gap);
-        for (index, row) in grid_render.iter().enumerate() {
-            let prefix = format!("{:0>width$} |", format!("{:0>width$}", index, width = horizontal_gap), width = horizontal_gap);
-            let row = row.iter().join(&vertical_seperator);
-            rendered_grid.push(prefix + &row);
+        // let column_gap = self.wordsearch.width.to_string().len() + 1;
+        let column_gap = 2;
+        let column_seperator = " ".repeat(column_gap);
+        for row in grid_render.iter() {
+            let row = row.iter().join(&column_seperator);
+            rendered_grid.push(row);
         }
 
         rendered_grid.join("\n")
+    }
+
+    /// Adds coordinate guides to the grid render
+    fn add_guides(&self, grid: String) -> String {
+        let mut rows = Vec::new();
+        let num_digits = self.wordsearch.height.to_string().len();
+
+        for (index, row) in grid.lines().enumerate() {
+            let new_row = format!("{:0num_digits$} |{}", index, row);
+            rows.push(new_row);
+        }
+
+        rows.join("\n")
     }
 
     /// Formats a final solution screen from a rendered bank and grid
@@ -97,6 +100,6 @@ Bank, top left = (0, 0): =====================================
 
     /// Renders a bank and grid, and then formats them to be displayed
     pub fn render(&self) -> String {
-        Self::format_render(self.render_grid(), self.render_bank())
+        Self::format_render(Self::add_guides(&self, self.render_grid()), self.render_bank())
     }
 }
